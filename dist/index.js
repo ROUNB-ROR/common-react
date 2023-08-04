e2def51e42009e6a1814b05d5a85294dc161e088
Andrii Katsubo <andrii.katsubo@gmail.com>
2023-08-04 10:28:21 +0300

    fixes

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,2 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = void 0;
-var _DeleteButton = _interopRequireDefault(require("./js/DeleteButton"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-var _default = _DeleteButton.default;
-exports.default = _default;
+import DeleteButton from './js/DeleteButton';
+export default DeleteButton;
