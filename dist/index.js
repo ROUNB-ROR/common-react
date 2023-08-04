58c7884cc0cc9553484c9dc3c87df2b01f61974f
Andrii Katsubo <andrii.katsubo@gmail.com>
2023-08-04 13:29:26 +0300

    Added Notifier

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 import DeleteButton from './DeleteButton';
 import FormItem from './FormItem';
 import Message from './Message';
+import * as Notifier from './Notifier';
 import Pagination from './Pagination';
-export { DeleteButton, FormItem, Message, Pagination };
+export { DeleteButton, FormItem, Message, Notifier, Pagination };
---
+++ src/Notifier/service.js
@@ -0,0 +1,16 @@
+// Service provides ability for the notifications to be handled
+export default class NotificationService {
+  //
+  static notify(header, message) {
+    const NOTIFICATION = {
+      id: NotificationService.newId += 1,
+      header,
+      message,
+    };
+
+    if (NotificationService.onNotify) NotificationService.onNotify(NOTIFICATION);
+  }
+}
+
+NotificationService.onNotify = null;
+NotificationService.newId = 1;
