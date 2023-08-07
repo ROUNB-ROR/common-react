2f04f1199f17d6eea9bf760bd977073756b82306
Andrii Katsubo <andrii.katsubo@gmail.com>
2023-08-07 11:48:51 +0300

    Path helper functions

---
+++ dist/helpers/path.js
@@ -0,0 +1,39 @@
+/**
+ * Get current path in encoded format for given request
+ *  */
+function getPathFromRequest(request) {
+  // Retrieving pathname and search params
+  const url = new URL(request.url);
+  const {
+    pathname,
+    search
+  } = url;
+  const encoded = encodeURIComponent(`${pathname}${search}`);
+  return encoded;
+}
+
+/**
+ * Get current path in encoded format for given location object
+ *  */
+function getPathFromLocation(location) {
+  // Retrieving pathname and search params
+  const {
+    pathname,
+    search
+  } = location;
+  const encoded = encodeURIComponent(`${pathname}${search}`);
+  return encoded;
+}
+
+/**
+ * Extracts return path from search params for given request
+ * @param {*} request
+ * @returns
+ */
+function getReturnPathFromRequest(request) {
+  // Retrieving pathname and search params
+  const url = new URL(request.url);
+  const returnTo = url.searchParams.get('returnTo');
+  return returnTo;
+}
+export { getPathFromRequest, getPathFromLocation, getReturnPathFromRequest };
