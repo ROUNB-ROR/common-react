8f2644d4c59c67d17e0dcac0b0f23121d7b6e876
Andrii Katsubo <andrii.katsubo@gmail.com>
2023-09-04 09:32:22 +0300

    fix

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -4,7 +4,7 @@ import AuthenticationStorage from '../authentication_storage';
 import { getPathFromRequest } from './path';
 
 // Returns redirect if user is not authenticated otherwise null
-export default function CheckAuth(request) {
+export default function checkAuth(request) {
   // If not authenticated then navigate to authentication page
   if (!AuthenticationStorage.isAuthenticated()) {
     // Retrieving encoded pathname
